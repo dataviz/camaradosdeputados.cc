a39a11667dc788a3249a8a89b027dbec612a3d8e
Leo Tartari <ltartari@gmail.com>
2013-06-26 10:38:17 -0300

    Initial commit. Rails + Teaser page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CamaradosdeputadosCc::Application.config.secret_key_base = '3f601c13f806f06465d5838c3faabfc2e7fd9d61e55c1fe94555900553114cdf6cf473e20d3dc3160bf25dd8b054a338fbfbb41ea55bb82f505adb957e8d9bbe'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+CamaradosdeputadosCc::Application.routes.draw do
+  root 'home#index'
+end
